637ffd2dca1d8c218c0568d03bd03d50e080736f
luisgomezcaballero <luis.gomez.caballero@gmail.com>
2018-02-21 17:25:12 +0100

    Project upload

---
+++ spring-batch-demo/src/main/java/com/luisgomezcaballero/springbatchdemo/MyJobConfig.java
@@ -0,0 +1,43 @@
+package com.luisgomezcaballero.springbatchdemo;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class MyJobConfig {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step myStep() {
+		return stepBuilderFactory.get("myStep").tasklet(new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
+				System.out.println("myTasklet");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+	
+	@Bean
+	public Job myJob() {
+		return jobBuilderFactory.get("myJob").start(myStep()).build();
+	}
+	
+}
